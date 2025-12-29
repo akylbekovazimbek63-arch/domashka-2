e12eb950398f40344957e323b0fd23ae2252f0a1
Azim <akylbekovazimbek63@gmail.com>
2025-12-29 19:45:57 +0600

    first commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,12 @@
+import MyInfo from './MyInfo';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Контрольная работа выполнена</h1>
+      <MyInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/MyInfo.jsx
@@ -0,0 +1,10 @@
+function MyInfo() {
+  return (
+    <div style={{ border: '2px solid #646cff', padding: '10px', marginTop: '20px', borderRadius: '8px' }}>
+      <h2>Это мой новый компонент!</h2>
+      <p>Все работает отлично.</p>
+    </div>
+  );
+}
+
+export default MyInfo;
